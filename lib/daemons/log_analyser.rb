a6286700ecc02bf121ddbe89c6604c1ef3e020f8
Kip Cole <kip@kip-macbook.refuge.noexpectations.com.au>
2009-05-06 09:28:03 +1000

    Make log daemon sensitive to Rails environment

--- lib/daemons/log_analyser.rb
+++ lib/daemons/log_analyser.rb
@@ -1,19 +1,27 @@
 #!/usr/bin/env ruby
 
 # Specify environment and load rails
-ENV["RAILS_ENV"] ||= "development" # Change: look at directory path and decide (based on our deployment strategy)
+# Has knowledge of the deployment strategy to work out which
+# environment to load.  We use a layout which is
+# /u/apps/{appname}/{environment}/..........
+env = File.basename(File.expand_path("#{__FILE__}/../../../.."))
+if ["staging", "production"].include?(env)
+  ENV["RAILS_ENV"] ||= env
+else
+  ENV["RAILS_ENV"] ||= "development"
+end
 require File.dirname(__FILE__) + "/../../config/environment"
 
 # Flag for File::Tail library
-$DEBUG = true
+# $DEBUG = true
 
 $RUNNING = true  
 Signal.trap("TERM") do
-  ActiveRecord::Base.logger.info "Trackster log analyser termination requested at #{Time.now}.\n"  
+  ActiveRecord::Base.logger.info "Trackster log analyser (#{ENV["RAILS_ENV"]}) termination requested at #{Time.now}.\n"  
   $RUNNING = false
 end
 
 log_analyser = LogAnalyserDaemon.new
-ActiveRecord::Base.logger.info "Trackster log analyser starting at #{Time.now}.\n"
+ActiveRecord::Base.logger.info "Trackster log analyser (#{ENV["RAILS_ENV"]}) starting at #{Time.now}.\n"
 log_analyser.log_analyser_loop
-ActiveRecord::Base.logger.info "Trackster log analyser ending at #{Time.now}.\n"
+ActiveRecord::Base.logger.info "Trackster log analyser (#{ENV["RAILS_ENV"]}) ending at #{Time.now}.\n"
