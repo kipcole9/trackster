8f1e137c140479c62c264262adf6db8094acefed
Kip Cole <kip@kip-macbook.refuge.noexpectations.com.au>
2009-05-07 12:46:37 +1000

    Fix name fields in user update form

--- app/views/users/edit.html.rb
+++ app/views/users/edit.html.rb
@@ -4,7 +4,8 @@ panel t('panels.user'), :flash => true, :display_errors => 'user'  do
     caerus_form_for @user, :html => { :multipart => true } do |user|
       fieldset t('.edit_user', :name => @user.name) do
         user.text_field       :login, :disabled => true
-        user.text_field       :name
+        user.text_field       :given_name
+        user.text_field       :family_name        
         user.text_field       :email
         user.select           :locale, [["English", 'en-US']]
         user.time_zone_select :timezone, time_zones_like(Time.zone), :default => Time.zone.name        
