0407a40e14bc09354073e7474bfec70b024beee7
Kip Cole <kip@kip-macbook.refuge.noexpectations.com.au>
2009-05-10 22:02:30 +1000

    Refactor log rotation processing in log analyser

--- lib/log_analyser_daemon.rb
+++ lib/log_analyser_daemon.rb
@@ -10,6 +10,56 @@ class LogAnalyserDaemon
     @last_log_entry  = [(Event.maximum(:tracked_at) || Time.at(0)), (Session.maximum(:ended_at) || Time.at(0))].max
     @nginx_log_dir   = '/usr/local/nginx/logs'
   end
+
+  def log_analyser_loop(options = {})
+    default_options = {:forward => 0}
+    options     = @options.merge(default_options).merge(options) 
+    log         = File::Tail::Logfile.open(log_file, options)
+    @log_inode  = File.stat(log_file).ino
+    log.interval            = 1     # Initial sleep interval when no data
+    log.max_interval        = 5     # Maximum sleep interval when no data
+    log.reopen_deleted      = true  # is default
+    log.reopen_suspicious   = true  # is default
+    log.suspicious_interval = 20    # When several loops of no data - like when logs rotated
+    
+    # Is called by log tailer when the log file is reopened (which happens after a series
+    # of EOF detections)
+    log.after_reopen do
+      if running?
+        ActiveRecord::Base.logger.debug "Log analyser has reopened #{log_file}"
+        check_if_log_was_rotated
+      else
+        ActiveRecord::Base.logger.info "Log analyser is terminating as requested (after log reopen)"
+        log.close
+        return
+      end
+    end
+    
+    # Main log loop
+    log.tail do |line|  
+      ActiveRecord::Base.connection.reconnect! unless ActiveRecord::Base.connected?      
+      entry = @log_parser.parse_entry(line)
+      if entry[:datetime]
+        if entry[:datetime] > @last_log_entry && entry[:url] =~ TRACK_PATTERN
+          @log_parser.save_web_analytics!(@web_analyser, entry) unless @web_analyser.is_crawler?(entry[:user_agent])
+        end
+      else
+        ActiveRecord::Base.logger.info "Skipping badly formatted log entry: #{line}"
+      end
+    end
+    
+    unless running?
+      ActiveRecord::Base.logger.info "Log analyser is terminating as requested (there may be unprocessed log entries)"
+      log.close
+      return
+    end    
+  end 
+
+  def running?
+    $RUNNING
+  end
+  
+private
   
   def log_file
     # The log file we're importing.  Implies knowledge of the deployment strategy and development
@@ -23,52 +73,17 @@ class LogAnalyserDaemon
         raise ArgumentError, "Unknown rails environment: #{Rails.env}"
       end
   end
-  
+
   def log_was_rotated?
     (new_file = File.stat(log_file).ino) != @log_inode ? new_file : false
   end
   
-  def log_analyser_loop(options = {})
-    default_options = {:forward => 0}
-    options = @options.merge(default_options).merge(options) 
-    log     = File::Tail::Logfile.open(log_file, options)
-    @log_inode  = File.stat(log_file).ino
-    log.interval            = 1     # Initial sleep interval when no data
-    log.max_interval        = 5     # Maximum sleep interval when no data
-    log.reopen_deleted      = true  # is default
-    log.reopen_suspicious   = true  # is default
-    log.suspicious_interval = 20    # When several loops of no data - like when logs rotated
-    
-    log.after_reopen do
-      if running?
-        ActiveRecord::Base.logger.debug "Log analyser has reopened #{log_file}"
-        if new_file_inode = log_was_rotated?
-          log.forward
-          @log_inode = new_file_inode
-        end
-      else
-        ActiveRecord::Base.logger.info "Log analyser is terminating"
-        log.close
-        return
-      end
+  def check_if_log_was_rotated  
+    if new_file_inode = log_was_rotated?
+      ActiveRecord::Base.logger.debug "Log analyser has detected a probable log rotation, moved to new logfile."
+      log.forward
+      @log_inode = new_file_inode
     end
-    
-    log.tail do |line|  
-      return unless running?
-      ActiveRecord::Base.connection.reconnect! unless ActiveRecord::Base.connected?      
-      entry = @log_parser.parse_entry(line)
-      if entry[:datetime]
-        if entry[:datetime] > @last_log_entry && entry[:url] =~ TRACK_PATTERN
-          @log_parser.save_web_analytics!(@web_analyser, entry) unless @web_analyser.is_crawler?(entry[:user_agent])
-        end
-      else
-        ActiveRecord::Base.logger.info "Skipping badly formatted log entry: #{line}"
-      end
-    end
-  end 
-
-  def running?
-    $RUNNING
   end
 
 end
