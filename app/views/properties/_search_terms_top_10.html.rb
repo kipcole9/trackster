1bcf203106448d70257b70c00f02fc4bd20e801b
Kip Cole <kip@kip-macbook.refuge.noexpectations.com.au>
2009-05-31 13:33:51 +1000

    table column width now percent based

--- app/views/properties/_search_terms_top_10.html.rb
+++ app/views/properties/_search_terms_top_10.html.rb
@@ -4,7 +4,7 @@ panel t('dashboards.search_terms_top_10'), :class => 'table'  do
       total_search_terms = @property.tracks.visits.between(Track.period_from_params(params)).first.visits
       store @property.tracks.visits.by(:search_terms).limit(10).between(Track.period_from_params(params)).all.to_table(:percent_of_visits => total_search_terms)
     rescue
-      store t('dashboards.no_search_terms_found')
+      h3 t('dashboards.no_search_terms_found')
     end
   end
 end
