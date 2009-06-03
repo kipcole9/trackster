dd354061c9111decc21bf3e328b48892fcbfada0
Kip Cole <kip@kip-macbook.refuge.noexpectations.com.au>
2009-06-03 13:21:17 +1000

    Don't graph visits if no visit data:

--- app/views/properties/_visits_graph.html.rb
+++ app/views/properties/_visits_graph.html.rb
@@ -1,7 +1,7 @@
 panel t('.visit_graph')  do
   block do
     visits = @property.tracks.visits.by(:date).between(Track.period_from_params(params)).all
-    if visits.empty?
+    if visits.empty? || (visits.size == 1 && visits.first.visits == "0")
       store t('.no_visits_yet')
     else
       store visits.to_chart(:visits, :date, :tooltip => "Date: #x_label#\nVisits: #val#", :regression => true)
