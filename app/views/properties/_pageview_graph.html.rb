1d367646cda11544a5b456440e835f5e281e976d
Kip Cole <kip@kip-macbook.refuge.noexpectations.com.au>
2009-06-03 13:26:05 +1000

    Update translation for no pageviews yet

--- app/views/properties/_pageview_graph.html.rb
+++ app/views/properties/_pageview_graph.html.rb
@@ -1,7 +1,7 @@
 panel t('.pageview_graph')  do
   block do
     page_views = @page_views || @property.tracks.page_views.by(:date).between(Track.period_from_params(params)).all
-    if page_views.empty?
+    if page_views.empty? || (page_views.size == 1 && page_views.first.page_views == "0")
       store t('.no_page_views_yet')
     else
       store page_views.to_chart(:page_views, :date, :tooltip => "Date: #x_label#\nViews: #val#", :regression => true)
