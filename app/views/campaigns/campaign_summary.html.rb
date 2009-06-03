aa4fde1523c7b0aef58d6b0efad9cf75c81e43b0
Kip Cole <kip@kip-macbook.refuge.noexpectations.com.au>
2009-06-04 08:49:44 +1000

    Refactor campaign views

--- app/views/campaigns/campaign_summary.html.rb
+++ app/views/campaigns/campaign_summary.html.rb
@@ -2,14 +2,14 @@ clear do
   column :width => 9, :class => 'main' do
     clear do
       column :width => 12 do
-        include "properties/campaign_impressions_graph"
+        include "campaigns/campaign_impressions_graph"
       end
     end
     clear do
       column :width => 12 do
-        include "properties/campaign_summary"
-        include "properties/campaign_impressions"                
-        include "properties/campaign_clicks"
+        include "campaigns/campaign_overview"
+        include "campaigns/campaign_impressions"                
+        include "campaigns/campaign_clicks"
       end
     end
   end
