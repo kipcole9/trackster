02793b36f0dd7d4f05009f7bb39fcfbba4ac2852
Kip Cole <kip@kip-macbook.refuge.noexpectations.com.au>
2009-06-03 15:47:22 +1000

    Modify store_location invocation strategy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < ActionController::Base
   before_filter     :force_login_if_required
   before_filter     :set_locale
   before_filter     :set_timezone
-  before_filter     :store_location, :only => [:index, :show]
+  before_filter     :store_location, :except => [:new, :create, :update, :destroy, :edit]
 
   layout            'application', :except => [:rss, :xml, :json, :atom, :vcf, :xls, :csv, :pdf, :js]
 
