f58f6480dd59acac340c3d06c545dea08e5e25fa
Kip Cole <kip@kip-macbook.refuge.noexpectations.com.au>
2009-06-05 10:31:26 +1000

    user tag delete fix

--- app/views/users/_tag.html.rb
+++ app/views/users/_tag.html.rb
@@ -9,6 +9,5 @@ with_tag(:li, :class => :tag, :id => id) do
   store link_to_remote(image_tag('/images/icons/cancel_bw.png'), 
         :url => relate_url(drop.id, :drop => element_from(drop), :drag => element_from(user)), 
         :method => :delete, :confirm => false,
-        :html => {:title => t(".remove_user_from_#{drop_class}")})  if current_user.is_administrator?
-  end
+        :html => {:title => t(".remove_user_from_#{drop_class}")}) if current_user.is_administrator?
 end
