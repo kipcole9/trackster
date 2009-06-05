7cda6bdd861b032bd6a35c80ccbcc822fb09ec94
Kip Cole <kip@kip-macbook.refuge.noexpectations.com.au>
2009-06-05 10:27:00 +1000

    Remove delete from user tag unless authorised

--- app/views/users/_tag.html.rb
+++ app/views/users/_tag.html.rb
@@ -7,7 +7,8 @@ user_text = user.name.blank? ? user.login : user.name
 with_tag(:li, :class => :tag, :id => id) do
   store link_to(user_text, user_url(user), :title => user.email)
   store link_to_remote(image_tag('/images/icons/cancel_bw.png'), 
-                      :url => relate_url(drop.id, :drop => element_from(drop), :drag => element_from(user)), 
-                      :method => :delete, :confirm => false,
-                      :html => {:title => t(".remove_user_from_#{drop_class}")})
+        :url => relate_url(drop.id, :drop => element_from(drop), :drag => element_from(user)), 
+        :method => :delete, :confirm => false,
+        :html => {:title => t(".remove_user_from_#{drop_class}")})  if current_user.is_administrator?
+  end
 end
