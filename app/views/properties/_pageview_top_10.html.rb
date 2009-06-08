f3e4fbbf82d027189b2d9e76025c43b113c4fcee
Kip Cole <kip@kip-macbook.refuge.noexpectations.com.au>
2009-06-08 21:16:59 +1000

    Update pageview top 10

--- app/views/properties/_pageview_top_10.html.rb
+++ app/views/properties/_pageview_top_10.html.rb
@@ -1,6 +1,6 @@
 panel t('dashboards.page_views_top_10'), :class => 'table'  do
   block do
-    page_views = @property.tracks.page_views.by(:url).limit(10).between(Track.period_from_params(params)).all
+    page_views = @property.tracks.page_views(:with_events).by(:url).limit(10).between(Track.period_from_params(params)).all
     if page_views.empty?
       h3 t('no_page_views_yet')
     else
