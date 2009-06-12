f0604ed1d57dfb0a765c001f6691362bfaed70ea
Kip Cole <kip@kip-macbook.refuge.noexpectations.com.au>
2009-06-12 15:08:32 +1000

    Add tooltip and regression to latency report

--- app/views/sites/_latency.html.rb
+++ app/views/sites/_latency.html.rb
@@ -3,7 +3,7 @@ panel t('sites.latency')  do
     if @latency.empty?
       h3 t('sites.no_latency_found')    
     else
-      store @latency.to_chart(:latency, :date)
+      store @latency.to_chart(:latency, :date, :tooltip => "Date: #x_label#\nLatency: #val# seconds", :regression => true)
     end
   end
 end
