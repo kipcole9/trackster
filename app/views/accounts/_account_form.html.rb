d2dc0278afdb433d12631deb8d717490671d8bb9
Kip Cole <kip@kip-macbook.refuge.noexpectations.com.au>
2009-06-18 13:58:28 +1000

    Show current logo in account edit

--- app/views/accounts/_account_form.html.rb
+++ app/views/accounts/_account_form.html.rb
@@ -6,6 +6,7 @@ caerus_form_for @account, :html => { :multipart => true } do |account|
   fieldset t('accounts.presentation') do
     account.text_field     :theme
     account.text_field     :chart_background_colour, :validate => :validations
+    store image_tag(@account.logo(:banner)) if @account.logo?
     account.file_field     :logo
   end
   submit_combo
