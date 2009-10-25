ad0939ddff7858fd6959008caa81120011372018
Kip Cole <kip@Kip-macbook.local>
2009-10-25 18:16:30 +0800

    Add property overview link in campaign summary (#6)

--- app/views/campaigns/_navigation.html.rb
+++ app/views/campaigns/_navigation.html.rb
@@ -1,12 +1,18 @@
-panel t('panels.navigation'), :class => 'accordian'  do
+panel t('panels.campaign_navigation'), :class => 'accordian'  do
   block do
     accordian do
-      accordian_item "#{image_tag '/images/icons/money_dollar.png'} Campaigns" do
+      accordian_item "#{image_tag '/images/icons/money_dollar.png'} Campaign" do
         p link_to("Overview")
         p link_to("Impressions")
         p link_to("Clicks-through")
         p link_to("Link destinations")
       end          
     end
+  
+    accordian do
+      accordian_item "#{image_tag '/images/icons/lightning.png'} Links" do
+        p link_to("Web Property Overview", property_report_path(@campaign.property, "overview"))
+      end          
+    end    
   end
 end
