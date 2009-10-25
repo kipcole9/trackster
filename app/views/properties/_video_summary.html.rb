5057e6c301b07d118abf62ea35b51e73d0c21ea9
Kip Cole <kip@Kip-macbook.local>
2009-10-25 21:33:45 +0800

    Display message if no videos have been played (#9)

--- app/views/properties/_video_summary.html.rb
+++ app/views/properties/_video_summary.html.rb
@@ -4,14 +4,17 @@ panel t('dashboards.video_summary', :name => params[:video] || t('videos.all')),
       h3 t('videos.no_video_plays_yet')
     end
   else
+    video_reports = 0
     @videos.each do |video|
-      block do
-        @events_summary = @property.video_summary(video, params).all
-        unless @events_summary.empty?
+      @events_summary = @property.video_summary(video, params).all
+      unless @events_summary.empty?
+        block do
           h3 I18n.t('videos.video', :name => video) unless @videos.size == 1
           store @events_summary.to_table
+          video_reports += 1
         end
       end
     end
+    block {h3 t('videos.no_video_plays_yet') if video_reports = 0}
   end
 end
