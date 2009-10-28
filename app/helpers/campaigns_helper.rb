4d69e5d089b80f9c570ec91c749097cad29f85c4
Kip Cole <kip@Kip-macbook.local>
2009-10-28 08:28:42 +0800

    Still not getting email chart I want..

--- app/helpers/campaigns_helper.rb
+++ app/helpers/campaigns_helper.rb
@@ -3,21 +3,21 @@ module CampaignsHelper
   # Show only the email clients that have 10% or more
   # collaps the others into "Other" category
   def collapse_impressions_data(impressions)
-    other_impressions = 0
+    other = 0
     new_impressions = []    
     total_impressions = impressions.sum(:impressions).to_f
     impressions.sort! {|a, b| a.impressions <=> b.impressions}.reverse!
 
     impressions.each do |i|
       if (i.impressions.to_f / total_impressions) >= 0.05
-        new_impressions << i.dup
+        new_impressions << i
       else
-        other_impressions =+ i.impressions
+        other =+ i.impressions
       end
     end
     other_impression = impressions.first.dup
     other_impression.browser = "Other"
-    other_impression.impressions = other_impressions
+    other_impression.impressions = other
     new_impressions << other_impression
     new_impressions
   end
