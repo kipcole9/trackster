fcc169ba11a0d8a0b4b1cd6c9dc632ec1b24edd8
Kip Cole <kip@Kip-macbook.local>
2009-11-01 10:46:03 +0800

    I18n the mailer templates and abstract the admin email and site host

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -1,23 +1,22 @@
 class UserMailer < ActionMailer::Base
   def signup_notification(user)
     setup_email(user)
-    @subject    += 'Please activate your new account'
-  
+    @subject    += I18n.t('please_activate')
     @body[:url]  =  activate_url(user.activation_code)
 
   end
   
   def activation(user)
     setup_email(user)
-    @subject    += 'Your account has been activated!'
+    @subject    += I18n.t('account_activated')
     @body[:url]  = root_url
   end
   
   protected
     def setup_email(user)
       @recipients  = "#{user.email}"
-      @from        = "activation@videoinemail.com"
-      @subject     = "[vietools activation] "
+      @from        = Trackster::Config.activation_email
+      @subject     = Trackster::Config.activation_subject
       @sent_on     = Time.now
       @body[:user] = user
     end
