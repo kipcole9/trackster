fb5bb62c41609a7f9ef855038dda51dad61f726e
Kip Cole <kip@Kip-macbook.local>
2009-11-01 15:41:47 +0800

    Update property navigation translation

--- app/views/campaigns/_edit_navigation.html.rb
+++ app/views/campaigns/_edit_navigation.html.rb
@@ -3,7 +3,7 @@ panel t('panels.action_sidebar'), :class => 'accordian'  do
     accordian do
       accordian_item "#{image_tag '/images/icons/lightning.png'} Actions" do
         p link_to t("campaigns.show_preview"), preview_property_campaign_path(@property, @campaign)
-        p link_to t("properties.edit_property", :name => @property.name), edit_property_path(@property)
+        p link_to t("properties.edit.edit_property", :name => @property.name), edit_property_path(@property)
       end          
     end    
   end
