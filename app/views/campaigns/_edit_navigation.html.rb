3324890634858c2957221e3d2f86f7a2ae7712fc
Kip Cole <kip@Kip-macbook.local>
2009-11-01 14:29:11 +0800

    Scope campaign preview with property

--- app/views/campaigns/_edit_navigation.html.rb
+++ app/views/campaigns/_edit_navigation.html.rb
@@ -1,5 +1,5 @@
 panel t('campaigns.edit_navigation'), :class => 'accordian'  do
   block do
-    p link_to t("campaigns.show_preview"), preview_campaign_path(@campaign)
+    p link_to t("campaigns.show_preview"), preview_property_campaign_path(@property, @campaign)
   end
 end
