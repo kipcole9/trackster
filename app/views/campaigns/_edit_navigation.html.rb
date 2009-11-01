90593af35e43272c5280cf71729ed207ebd92212
Kip Cole <kip@Kip-macbook.local>
2009-11-01 15:39:01 +0800

    Update navigation on campaign edit

--- app/views/campaigns/_edit_navigation.html.rb
+++ app/views/campaigns/_edit_navigation.html.rb
@@ -1,5 +1,10 @@
-panel t('campaigns.edit_navigation'), :class => 'accordian'  do
+panel t('panels.action_sidebar'), :class => 'accordian'  do
   block do
-    p link_to t("campaigns.show_preview"), preview_property_campaign_path(@property, @campaign)
+    accordian do
+      accordian_item "#{image_tag '/images/icons/lightning.png'} Actions" do
+        p link_to t("campaigns.show_preview"), preview_property_campaign_path(@property, @campaign)
+        p link_to t("properties.edit_property", :name => @property.name), edit_property_path(@property)
+      end          
+    end    
   end
 end
