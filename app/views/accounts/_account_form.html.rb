36a253a563b222f6cadafe090e4c64b2b6cbe90c
Kip Cole <kip@Kip-macbook.local>
2009-11-14 21:49:51 +0800

    Remove chart background field from accounts form

--- app/views/accounts/_account_form.html.rb
+++ app/views/accounts/_account_form.html.rb
@@ -5,7 +5,6 @@ caerus_form_for @account, :html => { :multipart => true } do |account|
   end
   fieldset t('accounts.presentation') do
     account.text_field     :theme
-    account.text_field     :chart_background_colour, :validate => :validations
     store image_tag(@account.logo(:banner)) if @account.logo?
     account.file_field     :logo
   end
