9b745e109cdcd8e3ac4d4742efe92abd7aff7718
Kip Cole <kip@Kip-macbook.local>
2010-02-15 09:18:37 +0800

    Mods to edit user form

--- vendor/plugins/authoriser/app/views/users/edit.html.rb
+++ vendor/plugins/authoriser/app/views/users/edit.html.rb
@@ -10,12 +10,6 @@ panel t('panels.user'), :flash => true, :display_errors => 'user'  do
         user.time_zone_select :timezone, time_zones_like(Time.zone), :default => Time.zone.name        
         user.file_field       :photo
       end
-      if current_user.has_role?(Role::ACCOUNT_ROLE)
-        fieldset t('.user_type') do
-          user.select :role, Role.available_roles(current_user), :selected => user.object.roles.first.id
-        end
-      end
-      possible_properties
       submit_combo
     end
   end
