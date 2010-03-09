a31f658b2d6f888686b26d1f5e97e9707b921ba3
Kip Cole <kip@Kip-macbook.local>
2010-03-09 11:28:23 +0800

    Update mih theme

--- public/themes/mih/_branding.html.rb
+++ public/themes/mih/_branding.html.rb
@@ -1,6 +1,6 @@
 clear do
   column :width => 12 do
-    with_tag(:div, :id => 'branding') do
+    with_tag(:div, :id => 'mih-branding') do
       store image_tag("#{current_theme_path}/brand.png")
 	  end
     with_tag(:div, :id => 'logo') do
