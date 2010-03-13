7624a51ae9402593e3d5f8e124479fccc2225c37
Kip Cole <kip@Kip-macbook.local>
2010-03-13 12:29:21 +0800

    Fix property list link to reporting overview

--- app/views/properties/_property_summary.html.rb
+++ app/views/properties/_property_summary.html.rb
@@ -3,7 +3,7 @@ clear do
     img (property.thumb? ? property.thumb(:thumb) : '/images/list_icons/property.png'), :class => :list_image
   end
   float :left do
-    h4 link_to(h(property.name), property_report_path(property, "overview"))
+    h4 link_to(h(property.name), property_report_path(property, "visit_overview"))
     p h(property.description), property_style(property)
     p link_to(property.url, property.url)
   end
