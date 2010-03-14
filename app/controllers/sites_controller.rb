8daa9d5c5618292d94ad95d9a6bb145426e39be4
Kip Cole <kip@Kip-macbook.local>
2010-03-14 08:18:18 +0800

    Normalise latency; roof of 30 seconds for reporting

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -5,6 +5,7 @@ class SitesController < ApplicationController
   def show
     date_range = (Date.today - 30).to_s(:db)
     @latency = Track.latency.order('date desc').by(:date).limit(30).filter("date > #{date_range}").all.sort{|a,b| a.date <=> b.date}
+    @latency.map{|t| t.latency = 30.seconds if t.latency.to_i > 30.seconds}
   end
   
 protected  
