e4ae3560c951f64be2c88d2b3ea08bef9326bedb
Kip Cole <kip@Kip-macbook.local>
2010-03-24 13:36:43 +0800

    OK, so don't stringify

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -8,7 +8,6 @@ module ReportsHelper
   end
 
   def report_params(options = {})
-    options.stringify_keys!
     ['campaign', 'time_group', 'from', 'to', 'by', 'period'].each do |arg|
       if ['from', 'to'].include?(arg)
         options[arg] = params[arg].to_date.to_s(:db) if params[arg] && !params['period']
