c875422bc36fac127fe0abf462928ff698b7ba87
Kip Cole <kip@Kip-macbook.local>
2010-04-19 20:04:32 +0800

    Undo cache location

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,7 @@ config.cache_classes = true
 
 # Full error reports are disabled and caching is turned on
 config.action_controller.consider_all_requests_local = false
-config.action_controller.perform_caching             = false
+config.action_controller.perform_caching             = true
 config.action_view.cache_template_loading            = true
 
 # See everything in the log (default is :info)
@@ -17,7 +17,7 @@ config.log_level = :info
 
 # Use a different cache store in production
 # config.cache_store = :mem_cache_store
-config.cache_store = :file, '/tmp/cache'
+# config.cache_store = :file, '/tmp/cache'
 
 # Enable serving of images, stylesheets, and javascripts from an asset server
 # config.action_controller.asset_host = "http://assets.example.com"
