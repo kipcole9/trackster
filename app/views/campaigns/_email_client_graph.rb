f60cc842fc090380fe2d0029a32f20c17fa56bb2
Kip Cole <kip@Kip-macbook.local>
2010-04-20 17:05:24 +0800

    Missed one email_client for graph

--- app/views/campaigns/_email_client_graph.rb
+++ app/views/campaigns/_email_client_graph.rb
@@ -5,7 +5,7 @@ panel t('campaigns.impressions_graph_by_browser')  do
       h3 t('no_impressions_yet')
     else
       new_impressions = collapse_impressions_data(impressions)
-      store new_impressions.to_chart(:impressions, :browser, :tooltip => "#label#\nImpressions: #val# (#percent#)", :type => OpenFlashChart::Pie)
+      store new_impressions.to_chart(:impressions, :email_client, :tooltip => "#label#\nImpressions: #val# (#percent#)", :type => OpenFlashChart::Pie)
     end
   end
 end
