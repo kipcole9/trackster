3c8282465ce0f9b1e52bbb38cbe24e53aee47b48
Kip Cole <kip@Kip-macbook.local>
2010-04-20 08:08:38 +0800

    Cache time dimension sidebar

--- app/views/reports/_time_period.html.rb
+++ app/views/reports/_time_period.html.rb
@@ -1,26 +1,28 @@
-panel t('navigation.time_dimension')  do
-  block do
-    accordion do
-      accordion_item "Time Period" do  
-        nav add_dimension(current_action, :period => 'today')
-        nav add_dimension(current_action, :period => 'yesterday')
-        nav add_dimension(current_action, :period => 'this_week')
-        nav add_dimension(current_action, :period => 'this_month')       
-        nav add_dimension(current_action, :period => 'this_year')  
-        nav add_dimension(current_action, :period => 'last_week')  
-        nav add_dimension(current_action, :period => 'last_month')  
-        nav add_dimension(current_action, :period => 'last_year')        
-        nav add_dimension(current_action, :period => 'last_30_days')
-        nav add_dimension(current_action, :period => 'last_12_months')
-        nav add_dimension(current_action, :period => 'lifetime')        
-      end
-      accordion_item "Time summarised by"  do
-        nav add_dimension(current_action, :time_group => 'date')      
-        nav add_dimension(current_action, :time_group => 'day_of_week')
-        nav add_dimension(current_action, :time_group => 'hour')
-        nav add_dimension(current_action, :time_group => 'day')
-        nav add_dimension(current_action, :time_group => 'month')       
-        nav add_dimension(current_action, :time_group => 'year')  
+cache "reports/navigation/time-dimensions/#{I18n.locale}" do
+  panel t('navigation.time_dimension')  do
+    block do
+      accordion do
+        accordion_item "Time Period" do  
+          nav add_dimension(current_action, :period => 'today')
+          nav add_dimension(current_action, :period => 'yesterday')
+          nav add_dimension(current_action, :period => 'this_week')
+          nav add_dimension(current_action, :period => 'this_month')       
+          nav add_dimension(current_action, :period => 'this_year')  
+          nav add_dimension(current_action, :period => 'last_week')  
+          nav add_dimension(current_action, :period => 'last_month')  
+          nav add_dimension(current_action, :period => 'last_year')        
+          nav add_dimension(current_action, :period => 'last_30_days')
+          nav add_dimension(current_action, :period => 'last_12_months')
+          nav add_dimension(current_action, :period => 'lifetime')        
+        end
+        accordion_item "Time summarised by"  do
+          nav add_dimension(current_action, :time_group => 'date')      
+          nav add_dimension(current_action, :time_group => 'day_of_week')
+          nav add_dimension(current_action, :time_group => 'hour')
+          nav add_dimension(current_action, :time_group => 'day')
+          nav add_dimension(current_action, :time_group => 'month')       
+          nav add_dimension(current_action, :time_group => 'year')  
+        end
       end
     end
   end
