fa47de6b0428c46084df4c929d6fe65faea43bc4
Kip Cole <kip@Kip-macbook.local>
2010-06-08 16:48:53 +0800

    Add new mih service logo

---
+++ doc/email tracking.rb
@@ -0,0 +1,11 @@
+<style>
+@media print{
+	#_t { background-image: url('https://a6.emltrk.com/0C7BC411?p');}
+} 
+div.OutlookMessageHeader, table.moz-email-headers-table, blockquote #_t {
+	background-image:url('https://a6.emltrk.com/0C7BC411?f')
+}
+</style>
+
+<div id="_t"></div>
+<img src="https://a6.emltrk.com/0C7BC411" style="display:none" width="1" height="1" border="0" />
