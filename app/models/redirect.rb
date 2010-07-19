f429108d1d02b72f2b1b4a20cb19e90894b8a9b5
Kip Cole <kip@n11648221207.netvigator.com>
2010-07-19 13:51:10 +0800

    Scope URL to account for a redirect

--- app/models/redirect.rb
+++ app/models/redirect.rb
@@ -11,7 +11,7 @@ class Redirect < ActiveRecord::Base
   
   validates_presence_of     :url
   validates_length_of       :url,     :within => 5..200
-  validates_uniqueness_of   :url
+  validates_uniqueness_of   :url,     :scope => :account_id
   validates_format_of       :url,     :with => /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/ix
   
   validates_numericality_of :value, :allow_nil => true
