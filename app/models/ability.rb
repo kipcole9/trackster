3add59a10695ec9bf840630956af67dd7f25a1ea
Kip Cole <kip@28.2.128.10.in-addr.arpa.noptr.antlabs.com>
2010-07-26 23:24:09 +0800

    Update abilities to reflect roles

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -10,6 +10,10 @@ class Ability
       can :read, [Property, Campaign, Content, Contact, Person, Organization]
       can :update, User, :id => user.id
     end
+    
+    can :update, Campaign if user.has_role?(:campaign_manager)
+    can :update, Contact  if user.has_role?(:crm_manager)
+    can :update, Content  if user.has_role?(:designer)
 
   end
   
