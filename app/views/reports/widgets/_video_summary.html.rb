9a84bf5aa569b7f1396c23ed4ab7515f8b0fb356
Kip Cole <kip@n119237229079.netvigator.com>
2010-10-24 12:41:31 +0800

    Bar and precentage for video views summary

--- app/views/reports/widgets/_video_summary.html.rb
+++ app/views/reports/widgets/_video_summary.html.rb
@@ -3,7 +3,8 @@ panel t('reports.name.video_summary', :name => params[:video] || t('videos.all')
     if @report.empty?
       h3 t('videos.no_video_plays_yet')
     else
-      store @report.to_table
+      total_views = @report.map{|v| v.video_views.to_i }.sum
+      store @report.to_table(:percent_of_video_views => total_views)
     end
   end
 end
