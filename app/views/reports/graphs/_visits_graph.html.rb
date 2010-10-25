c66d5579ccda775e823747f65e3786d2a87cd516
Kip Cole <kip@Kip-macbook.local>
2010-10-26 00:06:39 +0800

    Add weekend plotbands to visits chart

--- app/views/reports/graphs/_visits_graph.html.rb
+++ app/views/reports/graphs/_visits_graph.html.rb
@@ -4,7 +4,10 @@ panel t('reports.name.visits', :time_group => time_group_t, :time_period => time
     if @visits_graph.empty?
       h3 t('reports.no_visits_recorded')
     else
-      store @visits_graph.to_chart(:visits, time_group, :tooltip => "At: #x_label#\nVisits: #val#", :regression => true)
+      store @visits_graph.to_chart(:visits, time_group, 
+        :tooltip => "At: #x_label#\nVisits: #val#", 
+        :regression => true,
+        :weekend_plot_bands => true)
     end
   end
 end
