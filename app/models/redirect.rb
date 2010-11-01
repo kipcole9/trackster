fc4b79043560b0752d7f833eea770c51deb8ab50
Kip Cole <kip@Kip-macbook.local>
2010-11-01 12:09:11 +0800

    Detect document type for a redirect and define the appropriate category and action

--- app/models/redirect.rb
+++ app/models/redirect.rb
@@ -10,11 +10,16 @@ class Redirect < ActiveRecord::Base
   validates_length_of       :name,    :within => 1..250
   
   validates_presence_of     :url
-  validates_length_of       :url,     :within => 5..200
+  validates_length_of       :url,     :within => 5..250
   validates_uniqueness_of   :url,     :scope => :account_id
   validates_format_of       :url,     :with => /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/ix
   
   validates_numericality_of :value, :allow_nil => true
+  
+  DOWNLOAD_TYPES           = /\.(pdf|doc|xls|ppt|xlsx|pptx)\Z/
+  VIDEO_TYPES              = /\.(wmv|avi|mp4|m4v|ogg)\Z/
+  
+  attr_reader              :parsed_url
 
   named_scope :search, lambda {|criteria|
     search = "%#{criteria}%"
@@ -25,9 +30,23 @@ class Redirect < ActiveRecord::Base
     return nil if url.blank?
     absolute_url = get_absolute_url(base_url, url) 
     account = Account.current_account
+    category, action = category_and_action_from(url)
     redirect = account.redirects.find_by_url(absolute_url) ||
-               account.redirects.create!(:url => absolute_url, :name => redirect_name_from(link_content, absolute_url))
+               account.redirects.create!(
+                      :url        => absolute_url, 
+                      :name       => redirect_name_from(link_content, absolute_url),
+                      :category   => category,
+                      :action     => action
+               )
     redirect
+  end
+  
+  def category_and_action_from(url)
+    path = parsed_url(url).path
+    defaults = ['page', 'view']
+    doctype = path.split('/').last.split('.').last
+    doctype = 'html' if doctype.blank?
+    return I18n.t("doctypes.#{doctype}", :default => lambda{|z, y| defaults})
   end
   
   # Create a random token for the redirect url
@@ -44,13 +63,17 @@ class Redirect < ActiveRecord::Base
   def self.redirect_name_from(link_content, url)
     link_content.try(:strip).blank? ? name_from_url(url) : link_content.strip
   end
-    
+  
+  def parsed_url(url)
+    URI.parse(url)
+  end
+  
   def self.name_from_url(url)
-    path = URI.parse(url).path
+    path = parsed_url.path
   end
   
   def self.get_absolute_url(base_url, url)
-    if URI.parse(url).scheme
+    if parsed_url.scheme
       url
     else
       [base_url, url.without_slash].compact.join('/').gsub('//','/')
