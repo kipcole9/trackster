c29ed81a38c5fea61282d4e4dcdc8470fd1ae6bb
Kip Cole <kip@Kip-macbook.local>
2010-11-03 15:39:26 +0800

    Turn off banner caching for now

--- app/views/widgets/_branding.rb
+++ app/views/widgets/_branding.rb
@@ -1,13 +1,12 @@
-cache "branding/#{current_account['id']}/#{current_user ? current_user['id'] : 'login'}/#{I18n.locale}" do
+# cache "branding/#{current_account['id']}/#{current_user ? current_user['id'] : 'login'}/#{I18n.locale}" do
   column :width => 12 do
     with_tag(:div, :id => 'branding') do
       if current_account && current_account.logo?
         store image_tag(current_account.logo(:banner))
       else
-  	     h1 "#{page_title}"
-  	    # h1 link_to(Trackster::Config.banner, root_path)
+  	     h1 page_title
       end
     end
   end
-end
+# end
 
